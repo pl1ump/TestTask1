d386ad989c46bed188e24c0775d5cdf2600876ee
vladyslav <nnnnnniikttttto@gmail.com>
2025-08-23 12:17:49 +0200

    Initial Commit
    first commit, mvvm files created

---
+++ TestTask1/TestTask1/TestTask1App.swift
@@ -0,0 +1,17 @@
+//
+//  TestTask1App.swift
+//  TestTask1
+//
+//  Created by Vladick  on 23/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct TestTask1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
